dda1e8145656c731a4db8ccdd15d18e3304bceb2
Shibi1817 <61073758+Shibi1817@users.noreply.github.com>
2023-06-24 14:01:37 +0530

    Update PalindromeNumber.java

--- day2/PalindromeNumber.java
+++ day2/PalindromeNumber.java
@@ -4,14 +4,14 @@ public class PalindromeNumber {
 
 	public static void main(String[] args) {
 
-		int num=34343,res=0;
+		int num=34343,res=0,temp=num;
 		{
 			while(num!=0) {
 				int rem=num%10;
 				res=res*10+rem;
 				num=num/10;
 			}
-			if(num==res) {
+			if(temp==res) {
 				System.out.println("Palindrome Number");
 			}
 			else {
